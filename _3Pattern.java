002df3c1285f51123369116abe3f6d7946d8e3a3
deshmukhmahima32 <deshmukhmahima40@gmail.com>
2024-09-05 10:57:42 +0530

    Add files via upload

---
+++ _3Pattern.java
@@ -0,0 +1,26 @@
+public class _3Pattern {
+    public static void main(String [] args)
+    {
+        int i,j;
+        for(i=1;i<=5;i++){
+            //int k=5;
+            for(j=5;j>=i;j--){
+               // if(i%2==0)
+                
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++)
+
+            {
+
+                System.out.print(" "+"*");
+            }
+            System.out.println("");
+        }
+        }
+           // System.out.println("");
+        
+    }
+    
+
+        
