857c67e2b5dbb6e96ebd6b321d49396744fa26f3
deshmukhmahima32 <deshmukhmahima40@gmail.com>
2024-09-05 10:58:11 +0530

    Add files via upload

---
+++ _3EvenNo100.java
@@ -0,0 +1,30 @@
+public class _3EvenNo100 {
+    public static void main(String [] args)
+    {
+        int i;
+        int sum=0;
+        for(i=1;i<=10;i++)
+        {
+            if(i%2!=0)
+{
+           // System.out.println(i);
+            sum=sum+i;
+           }        
+              else
+              {
+                //System.out.println(i);
+              }
+        } 
+        System.out.println(sum);
+
+
+        }
+    
+    
+
+/*{ 
+    i++;
+    System.out.println(i-1);
+}*/
+    }
+
